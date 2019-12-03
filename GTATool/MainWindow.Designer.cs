0953cdccd5e1e99e0e5123b9aa6d361b7305b07b
Aleksander Evensen <ahse03@gmail.com>
2019-12-03 19:52:21 +0100

    Uploaded source code v1.0.0

---
+++ GTATool/MainWindow.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace GTATool {
+    partial class MainWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.aw_enable = new System.Windows.Forms.Button();
+            this.spg_enable = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.af_enable = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rText_console = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Walk (Hotkey: CTRL+ALT+F2)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dely until start";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "How long to press \"W\"";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(324, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Solo public generator(Hotkey: CTRL+ALT+L)";
+            // 
+            // aw_enable
+            // 
+            this.aw_enable.Location = new System.Drawing.Point(232, 85);
+            this.aw_enable.Name = "aw_enable";
+            this.aw_enable.Size = new System.Drawing.Size(75, 20);
+            this.aw_enable.TabIndex = 6;
+            this.aw_enable.Text = "Enable";
+            this.aw_enable.UseVisualStyleBackColor = true;
+            this.aw_enable.Click += new System.EventHandler(this.aw_enable_Click);
+            // 
+            // spg_enable
+            // 
+            this.spg_enable.Location = new System.Drawing.Point(17, 45);
+            this.spg_enable.Name = "spg_enable";
+            this.spg_enable.Size = new System.Drawing.Size(75, 23);
+            this.spg_enable.TabIndex = 7;
+            this.spg_enable.Text = "Enable";
+            this.spg_enable.UseVisualStyleBackColor = true;
+            this.spg_enable.Click += new System.EventHandler(this.spg_enable_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(251, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Anti AFK (Hotkey: CTRL+ALT+F1)";
+            // 
+            // af_enable
+            // 
+            this.af_enable.Location = new System.Drawing.Point(17, 139);
+            this.af_enable.Name = "af_enable";
+            this.af_enable.Size = new System.Drawing.Size(75, 23);
+            this.af_enable.TabIndex = 9;
+            this.af_enable.Text = "Enable";
+            this.af_enable.UseVisualStyleBackColor = true;
+            this.af_enable.Click += new System.EventHandler(this.af_enable_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.aw_enable);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(12, 308);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 130);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Under Development";
+            // 
+            // rText_console
+            // 
+            this.rText_console.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rText_console.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rText_console.ForeColor = System.Drawing.Color.ForestGreen;
+            this.rText_console.Location = new System.Drawing.Point(406, 9);
+            this.rText_console.Name = "rText_console";
+            this.rText_console.ReadOnly = true;
+            this.rText_console.Size = new System.Drawing.Size(382, 429);
+            this.rText_console.TabIndex = 11;
+            this.rText_console.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "GTA will freeze for 10 seconds";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(203, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "This will move your cursor in one direction";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rText_console);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.af_enable);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.spg_enable);
+            this.Controls.Add(this.label4);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "MainWindow";
+            this.Text = "GTA Tool";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button aw_enable;
+        private System.Windows.Forms.Button spg_enable;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button af_enable;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rText_console;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ GTATool/MainWindow.cs
@@ -0,0 +1,286 @@
+﻿using GlobalHotKey;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace GTATool {
+    public partial class MainWindow : Form {
+
+        // This is the hotkey manager this will store all the hotkeys that is used in the program
+        HotKeyManager hkManager;
+
+
+        // this defines all the different hotkeys
+        HotKey /*autoWalkKey,*/ soloPublicKey, afkKey;
+
+
+        // These are control variables when activating different parts of the program
+        bool awEnabled = false, spgEnabled = false, afEnabled = false;
+        
+        // Solo public control variable
+        bool isStartingSoloPublic = false;
+
+        // Anti-AFK control variable
+        bool isAntiAFKRunning = false;
+        // mouse thread
+        Thread MouseThread;
+
+
+        public MainWindow() {
+            InitializeComponent();
+
+
+            // Initialize the hotkey manager
+            hkManager = new HotKeyManager();
+
+
+            // set the solo public hotkey to CTRL + ALT + L
+            soloPublicKey = hkManager.Register(Key.L, System.Windows.Input.ModifierKeys.Control 
+                                                    | System.Windows.Input.ModifierKeys.Alt);
+            // set the ANTI-AFK hotkey to CTRL + ALT + F1
+            afkKey = hkManager.Register(Key.F1, System.Windows.Input.ModifierKeys.Control
+                                              | System.Windows.Input.ModifierKeys.Alt);
+
+
+            // Adding the medthod to call when the hotkeys are pressed.
+            hkManager.KeyPressed += HkManager_KeyPressed;
+
+
+            // This initialize a thread that is moving to mouse cursor(Used in ANTI-AFK)
+            MouseThread = new Thread(new ThreadStart(MouseThreadFunction));
+        }
+
+
+        // This medthod is called when you press a hotkey.
+        // it is also responsible for checking what hotkey was pressed and what to do when the correct hot key is pressed
+        private void HkManager_KeyPressed(object sender, KeyPressedEventArgs e) {
+
+            // Solo Public
+            if(e.HotKey.Key == Key.L) {
+                if(spgEnabled) {
+                    StartSoloPublic();
+                } else {
+                    WriteLine("Solo public hotkey isn't enabled");
+                }
+
+            // Anti-AFK
+            } else if(e.HotKey.Key == Key.F1) {
+                if(afEnabled) {
+                    ToggleAfk();
+                } else {
+                    WriteLine("Anti-AFK hotkey isn't enabled");
+                }
+            }
+        }
+
+        
+        //This method is called when the program closes to dispose and unregister the hotkeys
+        private void MainWindow_FormClosed(object sender,FormClosedEventArgs e) {
+            hkManager.Unregister(soloPublicKey);
+            hkManager.Unregister(afkKey);
+
+            hkManager.Dispose();
+        }
+
+
+        // These three methods is called when the enable buttons is pressed
+        private void aw_enable_Click(object sender, EventArgs e) {
+            //awEnabled = !awEnabled;
+            if(awEnabled) {
+                aw_enable.Text = "Disable";
+                //awHK = AutoWalkHK.Register(System.Windows.Input.Key.F5,System.Windows.Input.ModifierKeys.Control & System.Windows.Input.ModifierKeys.Alt)
+            } else {
+                aw_enable.Text = "Enable";
+                //AutoWalkHK.Unregister(awHK);
+            }
+        }
+
+        private void spg_enable_Click(object sender,EventArgs e) {
+            spgEnabled = !spgEnabled;
+            if(spgEnabled) {
+                spg_enable.Text = "Disable";
+            } else {
+                spg_enable.Text = "Enable";
+            }
+        }
+
+        private void af_enable_Click(object sender,EventArgs e) {
+            afEnabled = !afEnabled;
+            if(afEnabled) {
+                af_enable.Text = "Disable";
+            } else {
+                af_enable.Text = "Enable";
+            }
+            
+        }
+
+
+
+
+        #region Solo Public Starter
+
+
+        // This region is just used for suspending and unsuspending of processes
+        #region process suspension
+        [Flags]
+        public enum ThreadAccess : int {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+
+        private void SuspendProcess(Process process) {
+
+
+            foreach(ProcessThread pT in process.Threads) {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if(pOpenThread == IntPtr.Zero) {
+                    continue;
+                }
+
+                SuspendThread(pOpenThread);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        public void ResumeProcess(Process process) {
+
+
+            foreach(ProcessThread pT in process.Threads) {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if(pOpenThread == IntPtr.Zero) {
+                    continue;
+                }
+
+                var suspendCount = 0;
+                do {
+                    suspendCount = ResumeThread(pOpenThread);
+                } while(suspendCount > 0);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        #endregion
+
+
+        // This method will generate a solo public session
+        public void StartSoloPublic() {
+
+            // Get the gta process
+            Process GTAProcess = GetProcessStatus();
+
+
+            // if process is found do the code in between the blocks
+            if(GTAProcess != null) {
+                isStartingSoloPublic = true;
+
+                WriteLine("Creating Solo Public Session. This takes 10.5 seconds");
+                
+                // Suspend the gta process
+                SuspendProcess(GTAProcess);
+
+                // Wait for 10,5 seconds
+                Thread.Sleep(10500);
+
+                // Resume the process
+                ResumeProcess(GTAProcess);
+                // Write a sucess message to console
+                WriteLine("Solo Public created");
+
+                isStartingSoloPublic = false;
+            
+            // if process isn't found write it to the custom console
+            } else {
+                WriteLine("GTA proccess could not be found");
+            }
+        }
+
+
+        // this method is used to check if gta is running and if it is then return it
+        // otherwise Write error to console
+        private Process GetProcessStatus() {
+            try {
+                Process[] myProcesses = Process.GetProcessesByName("GTA5");
+
+                if(myProcesses.Length > 0) {
+                    WriteLine("GTA Process is running.");
+                    return myProcesses[0];
+                } else {
+                    WriteLine("GTA Process isn't running.");
+                    return null;
+                }
+            } catch(Exception ex) {
+                WriteLine("An Exception Occoured: " + ex.Message);
+                return null;
+            }
+        }
+        #endregion
+
+
+        #region Anit-AFK
+
+        // this method turns on and off the Anti-AFK Thread
+        public void ToggleAfk() {
+            if(isAntiAFKRunning) {
+                isAntiAFKRunning = false;
+                WriteLine("Afk Thread is stopping..");
+                // stop mouse thread
+                MouseThread.Join();
+
+                // Re-initialize the mouse thread.
+                MouseThread = new Thread(new ThreadStart(MouseThreadFunction));
+                
+            } else {
+                isAntiAFKRunning = true;
+                WriteLine("Afk Thread is starting..");
+                // start the mouse thread
+                MouseThread.Start();
+            }
+        }
+
+        // this method is the loop for the Anti-AFK program
+        // it will only move the cursor to the right
+        public void MouseThreadFunction() {
+            while(isAntiAFKRunning) {
+                // sleep for 5ms
+                Thread.Sleep(5);
+
+                // move cursor by 1 px to the right
+                Cursor.Position = new Point(Cursor.Position.X + 1, Cursor.Position.Y);
+            }
+        }
+
+
+        #endregion
+
+
+        // Writes a line to the console
+        private void WriteLine(string line) {
+            rText_console.Text += "  <GTATool> "+line + Environment.NewLine;
+        }
+    }
+}
